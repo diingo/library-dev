7c3c44c321bf4ee3b54250ac79eaf1585c5c772d
Gene Le <gene.gkl@gmail.com>
2014-01-20 21:56:13 -0600

    this file has edits to library.rb before code requires big changes - addition of new instance variables, etc

---
+++ library2-version-before-outline-change.rb
@@ -0,0 +1,57 @@
+class Library
+  def initialize(name)
+    @books = []
+    @name = name
+  end
+
+  def books
+  end
+
+  def list_books
+  end
+
+  def borrowed_books
+  end
+
+  def available_books
+  end
+
+  def add_book(book)
+    @books.push(book)
+    puts "Added the book, #{book.title}, to #{@name}'s list of books. The library's current book collection contains:  "
+    p @books
+  end
+
+  def check_out(user, book)
+  end
+
+  def check_in(book)
+  end
+end
+
+class Borrower
+  def initialize(name)
+    @name = name
+  end
+
+  def borrowed_books
+  end
+
+  def name
+  end
+
+  def borrowed_books_count
+  end
+
+  def borrowed_books_list
+  end
+end
+
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+end
